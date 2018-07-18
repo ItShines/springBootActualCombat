6bf34070773129510c685b9e93104d6308e4e6e3
tranquilityshine@163.com <tranquilityshine@163.com>
2018-07-18 17:38:37 +0800

    springboot

--- actualcombat/src/main/java/com/springboot/actualcombat/ActualcombatApplication.java
+++ actualcombat/src/main/java/com/springboot/actualcombat/ActualcombatApplication.java
@@ -3,6 +3,9 @@ package com.springboot.actualcombat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Future
+ */
 @SpringBootApplication
 public class ActualcombatApplication {
 
