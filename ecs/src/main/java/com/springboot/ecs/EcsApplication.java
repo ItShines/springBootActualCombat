45843851a74411d5a9f0d04f26c64ca497f4261c
tranquilityshine@163.com <tranquilityshine@163.com>
2018-09-20 15:57:56 +0800

    ecs

---
+++ ecs/src/main/java/com/springboot/ecs/EcsApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.ecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcsApplication.class, args);
+    }
+}
